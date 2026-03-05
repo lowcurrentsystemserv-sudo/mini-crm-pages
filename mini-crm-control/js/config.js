bf1eec00ddcffde996e917d57c6fb66147bfe80f
lowcurrentsystemserv-sudo <lowcurrentsystemserv@gmail.com>
2026-03-05 16:27:31 +0500

    Update config.js

--- mini-crm-control/js/config.js
+++ mini-crm-control/js/config.js
@@ -1,11 +1,11 @@
 export const CONFIG = {
+
   APP_NAME: "mini-crm-control",
 
-  // теперь API на том же домене
   API_BASE_URL: "/api",
 
-  // выключаем mock
   USE_MOCK: false,
 
-  REQUEST_TIMEOUT_MS: 12000,
+  REQUEST_TIMEOUT_MS: 12000
+
 };
