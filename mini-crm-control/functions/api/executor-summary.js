38f4ab354b60ef45981912a6301e6776d54fa7e2
lowcurrentsystemserv-sudo <lowcurrentsystemserv@gmail.com>
2026-03-14 13:46:53 +0500

    Update executor-summary.js

--- mini-crm-control/functions/api/executor-summary.js
+++ mini-crm-control/functions/api/executor-summary.js
@@ -4,6 +4,8 @@ export async function onRequestGet({ env }) {
   return json({
     ok: true,
     hasSessionsBinding: !!env.SESSIONS,
-    sessionsType: typeof env.SESSIONS
+    sessionsType: typeof env.SESSIONS,
+    hasSupabaseUrl: !!env.SUPABASE_URL,
+    hasServiceRoleKey: !!env.SUPABASE_SERVICE_ROLE_KEY
   });
 }
